e8d58c8607fe20d17b12df50753a170d381cf990
ssamkkim <ssamuel.kkim@gmail.com>
2022-07-11 09:30:25 -0700

    Basic positioning and button creations

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import './App.scss';
 
 function App() {
   return (
-    <div>
-      Hello
+    <div id="app">
+      <div id="calculator">
+        <div id="display"></div>
+        <button id="zero">0</button>
+        <button id="one">1</button>
+        <button id="two">2</button>
+        <button id="three">3</button>
+        <button id="four">4</button>
+        <button id="five">5</button>
+        <button id="six">6</button>
+        <button id="seven">7</button>
+        <button id="eight">8</button>
+        <button id="nine">9</button>
+        <button id="clear">AC</button>
+        <button id="add">+</button>
+        <button id="subtract">-</button>
+        <button id="multiply">x</button>
+        <button id="divide">/</button>
+        <button id="equals">=</button>
+        <button id="decimal">.</button>
+      </div>
     </div>
+    
   );
 }
 
