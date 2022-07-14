6ff51e78cc9af2985bc317263667d8a74e1985f8
ssamkkim <ssamuel.kkim@gmail.com>
2022-07-14 01:30:06 -0700

    Added decimal implementation

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends React.Component {
     this.handleNum = this.handleNum.bind(this);
     this.handleClear = this.handleClear.bind(this);
     this.handleOperator = this.handleOperator.bind(this);
-  };
+    this.handleEq = this.handleEq.bind(this);
+    this.handleDec = this.handleDec.bind(this);
+  };;
 
   handleNum(e) {
     if(e.target.value === "0" && this.state.formula === "0") {
@@ -26,13 +28,19 @@ class App extends React.Component {
           output: e.target.value,
         }));
       } else {
-        this.setState(prevState => ({
-          formula: prevState.formula + e.target.value,
-          output: e.target.value,
-        }));
+        if(/[0-9]/.test(this.state.output)) {
+          this.setState(prevState => ({
+            formula: prevState.formula + e.target.value,
+            output: prevState.formula + e.target.value,
+          }));
+        } else {
+          this.setState(prevState => ({
+            formula: prevState.formula + e.target.value,
+            output: e.target.value,
+          }));
+        }
       }
     }
-    
   }
 
   handleClear() {
@@ -42,18 +50,37 @@ class App extends React.Component {
     })
   };
 
-  handleOperator() {
+  handleOperator(e) {
+    this.setState(prevState => ({
+      formula: prevState.formula + e.target.value,
+      output: e.target.value
+    }));
+  }
 
+  handleEq(e) {
+    let str = this.state.formula.replace(/[^-()\d/*+.]/g, '');
+    let formulaAns = eval(str);
+    this.setState(prevState => ({
+      formula: formulaAns,
+      output: formulaAns
+    }));
+  }
+
+  handleDec(e) {
+    if(!this.state.output.includes(".")) {
+      this.setState(prevState => ({
+        formula: prevState.formula + e.target.value,
+        output: prevState.output + e.target.value
+      }));
+    } 
   }
 
   render(){
     return (
       <div id="app">
         <div id="calculator">
-          <div id="display">
-            <div id="formula">{this.state.formula}</div>
-            <div id="output">{this.state.output}</div>
-          </div>
+        <div id="formula">{this.state.formula}</div>
+          <div id="display">{this.state.output}</div>
           <button id="zero" value="0" onClick={this.handleNum}>0</button>
           <button id="one" value="1" onClick={this.handleNum}>1</button>
           <button id="two" value="2" onClick={this.handleNum}>2</button>
@@ -69,8 +96,8 @@ class App extends React.Component {
           <button id="subtract" value="-" onClick={this.handleOperator}>-</button>
           <button id="multiply" value="*" onClick={this.handleOperator}>x</button>
           <button id="divide" value="/" onClick={this.handleOperator}>/</button>
-          <button id="equals" value="=" onClick={this.handleClick}>=</button>
-          <button id="decimal" value="." onClick={this.handleClick}>.</button>
+          <button id="equals" value="=" onClick={this.handleEq}>=</button>
+          <button id="decimal" value="." onClick={this.handleDec}>.</button>
         </div>  
       </div>
       
